cdb4b7a9d52f33a713cda2d66dd287396a2aa492
Damian Montero <photoca@hotmail.com>
2020-08-23 12:33:13 -0400

    Fixing bug when first loading up total and renaming divs

--- code.js
+++ code.js
@@ -1,23 +1,26 @@
 let t = new Date();
 let today = t.getMonth()+1 + "/" + t.getDate() + "/" + t.getFullYear();
 let MonthlyTotal = localStorage.getItem("MonthlyTotal") ? Number(localStorage.getItem("MonthlyTotal")) : 0;
+let LastMonthTotal = localStorage.getItem("MonthlyTotal") ? Number(localStorage.getItem("MonthlyTotal")) : 0;
 let EntryList  = localStorage.getItem("EntryList") ? localStorage.getItem("EntryList") : "";
 let currentTotal = 0;
 let CurrentMonthSaved  = localStorage.getItem("CurrentMonthSaved") ? localStorage.getItem("CurrentMonthSaved") : t.getMonth()+1;
 
 if (CurrentMonthSaved != t.getMonth()+1) {
-    // New month. Do something later
+    // New month! Copy the monthly total to "LastMonthTotal" and start from scratch.
     console.log("new month!")
+    LastMonthTotal = MonthlyTotal;
+    MonthlyTotal = 0;
 }
 
-let currentTotalDiv = document.getElementsByClassName("Calculator-currentOperand")[0];
-let MonthlyTotalDiv = document.getElementsByClassName("Calculator-equals")[0].childNodes[1];
-let EntryListContainer = document.getElementsByClassName("Calculator-expressions")[0];
-let EntryListDiv = document.getElementsByClassName("Calculator-expressionsOverflow")[0];
+
+let currentTotalDiv = document.getElementsByClassName("CurrentTotal")[0];
+let MonthlyTotalDiv = document.getElementsByClassName("MonthlyTotal")[0].childNodes[1];
+MonthlyTotalDiv.innerHTML = "$" + MonthlyTotal + ".00";
+let EntryListContainer = document.getElementsByClassName("EntryListContainer")[0];
+let EntryListDiv = document.getElementsByClassName("EntryList")[0];
 EntryListDiv.innerHTML = EntryList;
 EntryListContainer.scrollTop = EntryListContainer.scrollHeight; // Scroll the list to the bottom
-
-console.log("Today",today);
 
 let calculatorbuttons = document.getElementsByClassName('Calculator-button');
 for (let i=0;i< calculatorbuttons.length; i++) {
