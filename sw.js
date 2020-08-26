91a16bf90b344e92e698990364536105e1a6e096
Henrique Lopes <hcmlopes@gmail.com>
2020-08-26 17:42:26 -0400

    🚑 Fixes service worker

--- sw.js
+++ sw.js
@@ -1,22 +1,32 @@
+const CACHE = 'cache-first'
+
 self.addEventListener('install', function (event) {
   event.waitUntil(
-    caches.open('sw-cache').then(function (cache) {
+    caches.open(CACHE).then(function (cache) {
       return cache.addAll(
         [
-          '/style.css',
-          '/code.js',
-          '/index.html'
+          './'
         ]
-      );
+      )
     })
   );
 });
 
-
 self.addEventListener('fetch', function (event) {
-  event.respondWith(
-    caches.match(event.request).then(function (response) {
-      return response || fetch(event.request);
+  if (navigator.onLine) {
+    fetch(event.request).then(function (response) {
+      caches.open(CACHE).then(function (cache) {
+        cache.put(event.request, response.clone());
+        return response;
+      })
     })
-  );
+  } else {
+    event.respondWith(
+      caches.open(CACHE).then(function (cache) {
+        return cache.match(event.request).then(function (response) {
+          return response
+        });
+      })
+    );
+  }
 });
